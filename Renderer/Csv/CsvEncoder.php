920ef7aacd8b1c1e1bf4accc71895dd2ddad6c36
Yosef Deray <yosefderay770@gmail.com>
2013-01-19 21:13:55 -0500

    added a csv encoder and its tests

---
+++ Renderer/Csv/CsvEncoder.php
@@ -0,0 +1,97 @@
+<?php
+namespace Yjv\Bundle\ReportRenderingBundle\Renderer\Csv;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CsvEncoder {
+
+	protected $options = array();
+	protected $optionsResolver;
+	
+	public function __construct(array $options = array()) {
+		
+		$this->options = $options;
+		$this->optionsResolver = new OptionsResolver();
+		$this->optionsResolver
+			->setRequired(array(
+				'delimiter',
+				'enclosure',
+				'encloseAll',
+				'printNull',
+				'lineEnding'
+			))
+			->setDefaults(array(
+			
+				'delimiter' => ',',
+				'enclosure' => '"',
+				'encloseAll' => false,
+				'printNull' => false,
+				'lineEnding' => PHP_EOL
+			))
+			->setAllowedTypes(array(
+				'delimiter' => 'string',
+				'enclosure' => 'string',
+				'encloseAll' => 'bool',
+				'printNull' => 'bool',
+				'lineEnding' => 'string'
+			))
+		;
+	}
+	
+	public function setOption($name, $value) {
+		
+		$this->options[$name] = $value;
+		return $this;
+	}
+	
+	public function getOptions() {
+		
+		return $this->optionsResolver->resolve($this->options);
+	}
+	
+	public function encode(array $data) {
+		
+		$options = $this->getOptions();
+		
+		$output = array();
+		foreach ($data as $row) {
+			
+			$output[] = $this->rowToCsv($row, $options);
+		}
+		
+		return implode($options['lineEnding'], $output);
+	}
+	
+	
+	protected function rowToCsv(array $row, array $options) {
+	
+		$enclosure = $options['enclosure'];
+		$delimiter = $options['delimiter'];
+		$delimiterEsc = preg_quote($delimiter, '/');
+		$enclosureEsc = preg_quote($enclosure, '/');
+	
+		$output = array();
+		foreach ($row as $field) {
+			 
+			if ($field === null && $options['printNull']) {
+				 
+				$output[] = 'NULL';
+				continue;
+			}
+	
+			//allows objects with __toString functionality to be used here
+			$field = (string)$field;
+			 
+			// Enclose fields containing $delimiter, $enclosure or whitespace
+			if ( $options['encloseAll'] || preg_match( "/(?:${delimiterEsc}|${enclosureEsc}|\s)/", $field ) ) {
+				 
+				$output[] = $enclosure . str_replace($enclosure, '\\' . $enclosure, $field) . $enclosure;
+			}else {
+				$output[] = $field;
+			}
+		}
+		
+		return implode( $delimiter, $output );
+	}
+	
+}
---
+++ Tests/Renderer/Csv/CsvEncoderTest.php
@@ -0,0 +1,89 @@
+<?php
+namespace Yjv\Bundle\ReportRenderingBundle\Tests\Renderer\Csv;
+
+use Yjv\Bundle\ReportRenderingBundle\Renderer\Csv\CsvEncoder;
+
+class CsvEncoderTest extends \PHPUnit_Framework_TestCase{
+
+	protected $encoder;
+	
+	public function setUp() {
+		
+		$this->encoder = new CsvEncoder();
+	}
+	
+	public function testOptions(){
+		
+		$options = $this->encoder->getOptions();
+		
+		$encoder = new CsvEncoder(array('lineEnding' => 'r'));
+		$this->assertNotEquals($options, $encoder->getOptions());
+		
+		$this->assertSame($this->encoder, $this->encoder->setOption('lineEnding', 'r'));
+		$this->assertEquals($encoder->getOptions(), $this->encoder->getOptions());
+		$this->assertNotEquals($options, $this->encoder->getOptions());
+	}
+	
+	/**
+     * @dataProvider encodeProvider
+     */
+    public function testEncode($data, $expectedCsv) {
+		
+		$this->assertEquals($expectedCsv, $this->encoder->encode($data));
+	}
+	
+	public function encodeProvider() {
+		
+		return array(
+			array(array(
+				array('column1', 'column2'),
+				array('column1', 'column2')
+			), 'column1,column2'.PHP_EOL.'column1,column2'),
+			array(array(
+				array('column 1', 'column 2'),
+				array('column1', 'column2')
+			), '"column 1","column 2"'.PHP_EOL.'column1,column2'),
+			array(array(
+				array('column1', 'column2'),
+				array(null, 'column2')
+			), 'column1,column2'.PHP_EOL.',column2')
+		);
+	}
+	
+	/**
+     * @dataProvider encodeWithDifferentOptionsProvider
+     */
+	public function testEncodeWithDifferentOptions($data, $expectedCsv, $optionName, $optionValue) {
+		
+		$this->assertEquals($expectedCsv, $this->encoder
+				->setOption($optionName, $optionValue)
+				->encode($data)
+		);
+	}
+
+	public function encodeWithDifferentOptionsProvider() {
+		
+		return array(
+			array(array(
+				array('column1', 'column2'),
+				array('column1', 'column2')
+			), 'column1kcolumn2'.PHP_EOL.'column1kcolumn2', 'delimiter', 'k'),
+			array(array(
+				array('column 1', 'column 2'),
+				array('column1', 'column2')
+			), 'ecolumn 1e,ecolumn 2e'.PHP_EOL.'column1,column2', 'enclosure', 'e'),
+			array(array(
+				array('column1', 'column2'),
+				array('column1', 'column2')
+			), '"column1","column2"'.PHP_EOL.'"column1","column2"', 'encloseAll', true),
+			array(array(
+				array('column1', 'column2'),
+				array(null, 'column2')
+			), 'column1,column2'.PHP_EOL.'NULL,column2', 'printNull', true),
+			array(array(
+				array('column1', 'column2'),
+				array('column1', 'column2')
+			), 'column1,column2ecolumn1,column2', 'lineEnding', 'e'),
+		);
+	}
+}
