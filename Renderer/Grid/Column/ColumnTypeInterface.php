357a36e8ba0c8d6ad7e89c9b047aed26e60cc74a
Yosef Deray <yosefderay770@gmail.com>
2013-01-26 20:03:13 -0500

    added the columntypeinterface and a registry class for them

---
+++ Renderer/Grid/Column/ColumnTypeInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Yjv\Bundle\ReportRenderingBundle\Renderer\Grid\Column;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+interface ColumnTypeInterface {
+
+	public function buildColumn(ColumnInterface $column, array $options);
+	public function createColumn(array $options = array());
+	public function getName();
+	public function getParent();
+	public function setDefaultOptions(OptionsResolverInterface $resolver);
+}
