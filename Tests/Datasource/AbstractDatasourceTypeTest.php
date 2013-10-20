8a053c8a9c56cc0887d8b5d9a042af44e3b01d08
Yosef Deray <yosefderay770@gmail.com>
2013-10-20 00:24:37 -0400

    added test for abstractdatasourcetype

---
+++ Tests/Datasource/AbstractDatasourceTypeTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Yjv\ReportRendering\Tests\Datasource;
+
+use Yjv\ReportRendering\Datasource\AbstractDatasourceType;
+
+use Mockery;
+
+class AbstractDatasourceTypeTest extends \PHPUnit_Framework_TestCase
+{
+    protected $type;
+    
+    public function setUp()
+    {
+        $this->type = new TestAbstractDatasourceType();
+    }
+
+    public function testCreateBuilder()
+    {
+        $this->type->createBuilder(Mockery::mock('Yjv\ReportRendering\Factory\TypeFactoryInterface'), array());
+    }
+    
+    public function testSetDefaultOptions()
+    {
+        $this->type->setDefaultOptions(Mockery::mock('Symfony\Component\OptionsResolver\OptionsResolverInterface'));
+    }
+    
+    public function testBuild()
+    {
+        $this->type->build(Mockery::mock('Yjv\ReportRendering\Datasource\DatasourceBuilderInterface'), array());
+        $this->type->buildDatasource(Mockery::mock('Yjv\ReportRendering\Datasource\DatasourceBuilderInterface'), array());
+    }
+    
+    public function testGetParent()
+    {
+        $this->assertEquals('datasource', $this->type->getParent());
+    }
+}
+
+class TestAbstractDatasourceType extends AbstractDatasourceType
+{
+    public function getName(){
+        
+        return 'test';
+    }
+}
