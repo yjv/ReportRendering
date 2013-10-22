7127b8fdbe4aff780d20c70118a87ce71321fe48
Yosef Deray <yosefderay770@gmail.com>
2013-10-21 23:02:53 -0400

    removed unused use statement

--- Datasource/DatasourceFactoryBuilder.php
+++ Datasource/DatasourceFactoryBuilder.php
@@ -3,8 +3,6 @@ namespace Yjv\ReportRendering\Datasource;
 
 use Yjv\ReportRendering\Datasource\DatasourceFactory;
 
-use Yjv\ReportRendering\DataTransformer\DataTransformerRegistry;
-
 use Yjv\TypeFactory\AbstractTypeFactoryBuilder;
 
 class DatasourceFactoryBuilder extends AbstractTypeFactoryBuilder
